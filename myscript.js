e335c826eb5f34ab736fb61907113aeb6a7b17ac
Andersonps81 <anderson07121981@gmail.com>
2022-08-04 23:51:00 -0300

    salvei o script externo adicionado

---
+++ myscript.js
@@ -0,0 +1,8 @@
+var ing = document.querySelector("ul").children[2].innerHTML;
+
+var lista = document.querySelector("ul");
+var novoItem = document.createElement("li");
+
+lista.appendChild(novoItem);
+document.querySelector("ul").children[3].innerHTML= "Azeitonas";
+console.log(ing);
